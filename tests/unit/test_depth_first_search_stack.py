3a02dc95b182a5218f6770ed763ffb8d11bb0c75
Craig Astill <craig@Craigs-MacBook-Air.local>
2024-01-14 09:25:48 +0000

    chore: RED: Pull in real data into dfs stack test.

--- tests/unit/test_depth_first_search_stack.py
+++ tests/unit/test_depth_first_search_stack.py
@@ -1,5 +1,11 @@
 """Tests for a Depth First Search Solution via a stack."""
 from src.tree_search.depth_first_search_stack import dfs
+from src.tree_search.main import (
+    read_file_contents,
+    parse_file_contents,
+)
+
+DATA = parse_file_contents(read_file_contents())
 
 
 class TestDepthFirstSearchStack:
@@ -38,3 +44,22 @@ class TestDepthFirstSearchStack:
             "E": 0.07,
         }
         assert dfs(graph, "A") == exp
+
+
+    def test_depth_first_search_real_data(self):
+        """Test DFS on the real data."""
+        exp = {
+            "Ethical Global Fund": 1,
+            "Fund B": 0.2,
+            "Fund C": 0.5,
+            "Fund D": 0.35,
+            "Fund E": 0.035,
+            "MicroFit": 0.1,
+            "GreenCo": 0.06,
+            "GrapeCo": 0.347,
+            "SolarCorp": 0.028,
+            "SpaceY": 0.105,
+            "BeanzRUS": 0.21,
+            "GoldenGadgets": 0.15,
+        }
+        assert dfs(DATA, "Ethical Global Fund") == exp
