9b0e03b206a39c529d10ab703df1a5a1f44cadb1
Craig Astill <craig@Craigs-MacBook-Air.local>
2024-01-14 08:52:19 +0000

    chore: GREEN: Implement DFS Stack internals.

--- src/tree_search/depth_first_search_stack.py
+++ src/tree_search/depth_first_search_stack.py
@@ -1,4 +1,27 @@
 """Depth First Search via a stack."""
 
 def dfs(graph: dict, node: str) -> list:
-    return []
+    """Depth First Search via a stack, which returns all nodes instead
+    of a list of _"first-seen"_ eg.
+
+           A
+         B   C
+       C  D
+           E
+
+    - Expect: ["A", "C", "B", "D", "E", "C", "C"].
+    - A stack-based DFS with memory: ["A", "C", "B", "D", "E"].
+
+    :param dict graph: Dict of graph keys and sub lists.
+    :param str node: Root node to start searching from.
+    :returns: list of Nodes.
+    """
+    ret_val = []
+    s = []
+    s.extend(node)
+    while s:
+        node = s.pop()
+        ret_val.append(node)
+        for node in graph.get(node, []):
+            s.append(node)
+    return ret_val
