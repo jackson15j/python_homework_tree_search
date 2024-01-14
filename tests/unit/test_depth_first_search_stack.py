7f38782af4aa19dd9fa820a5c1615810028d341e
Craig Astill <craig@Craigs-MacBook-Air.local>
2024-01-14 08:55:11 +0000

    chore: RED: Add name/weight key's to test data.

--- tests/unit/test_depth_first_search_stack.py
+++ tests/unit/test_depth_first_search_stack.py
@@ -4,11 +4,19 @@ from src.tree_search.depth_first_search_stack import dfs
 
 class TestDepthFirstSearchStack:
     def test_depth_first_search_simple_graph(self):
-        """Initial test to do DFS on an arbitrary graph."""
+        """Initial test to do DFS on an arbitrary graph that mimics
+        expected graph internals.
+        """
         graph = {
-            "A": ["B", "C"],
-            "B": ["C", "D"],
-            "D": ["C", "E"],
+            "A": [{"name": "B", "weight": 0.2}, {"name": "C", "weight": 0.8}],
+            "B": [{"name": "C", "weight": 0.3}, {"name": "D", "weight": 0.7}],
+            "D": [{"name": "C", "weight": 0.5}, {"name": "E", "weight": 0.5}],
         }
-        exp = ["A", "C", "B", "D", "E", "C", "C"]
+        exp = {
+            "A": 1,
+            "B": 0.2,
+            "C": 0.93,
+            "D": 0.14,
+            "E": 0.07,
+        }
         assert dfs(graph, "A") == exp
