c42fda039851c3f78ebce7150b068d990cfe5fa9
Craig Astill <craig@Craigs-MacBook-Air.local>
2024-01-14 11:28:46 +0000

    chore: REFACTOR: Clean up docstrings + plumb in main functions.

--- src/tree_search/main.py
+++ src/tree_search/main.py
@@ -33,16 +33,42 @@ def parse_file_contents(json_str: str) -> dict:
 
 
 def get_unique_companies_without_funds(weighted_funds: dict[str, float]) -> list:
+    """Return a list of companies with the Fund names stripped out.
+
+    :param dict[str, float] weighted_funds: dict of Company/Fund names
+        as the key and cumulative weights as the values.
+    :returns list: List of company names (minus fund names).
+    """
+    # NOTE: This obviously relies on the Funds having `Fund` in their
+    # name. Without this assumption, I would have to look at the
+    # original data and keep a set (python's de-duped list built-in)
+    # of all names that had a `holdings` key aswell. Then I could
+    # create a new list by checking the name is not in the list of
+    # Fund names.
     return [x for x in weighted_funds if "Fund" not in x]
 
 
 def is_company_under_fund(fund_data: dict[str, float], company: str) -> bool:
+    """Return bool of company in list under the Fund data.
+
+    :param dict[str, float] fund_data: dict of Company/Fund names
+        as the key and cumulative weights as the values.
+    :param str company: Company to check presence for.
+    :returns bool:
+    """
     return company in fund_data
 
 
 def get_company_investment_percentage_under_fund(
         fund_data: dict[str, float], company: str
 ) -> float:
+    """Return percentage of investment into company under the Fund.
+
+    :param dict[str, float] fund_data: dict of Company/Fund names
+        as the key and cumulative weights as the values.
+    :param str company: Company to get investment percentage for.
+    :returns float:
+    """
     return fund_data.get(company, 0.0) * 100
 
 
@@ -63,10 +89,10 @@ def main():
     # Answer: True, 15.0%.
     print(
         f"Is {ROGUE_COMPANY!r} under {ROOT_FUND!r}?:\n\t"
-        f"{ROGUE_COMPANY in companies}.\n"
+        f"{is_company_under_fund(output, ROGUE_COMPANY)}.\n"
         f"...And if so, what percentage of investment has gone to "
         f"{ROGUE_COMPANY!r}?:\n\t"
-        f"{output.get(ROGUE_COMPANY, 0) * 100}%"
+        f"{get_company_investment_percentage_under_fund(output, ROGUE_COMPANY)}%"
     )
 
 if __name__ == "__main__":
